3a9fd9713297dd0c94f4b4d9204b50c6c341ed47
YS-2357 <joungyoungsun20@gmail.com>
2025-07-09 08:04:18 +0000

    feat: 제품이미지 배경자동생성 기능 1차 완료 #11

--- backend/image_generator/image_generator_main.py
+++ backend/image_generator/image_generator_main.py
@@ -4,7 +4,7 @@ import sys
 
 from utils.logger import get_logger
 from .core.image_loader import ImageLoader
-from .core.background_handler import Txt2ImgGenerator, BackgroundHandler
+from .core.background_handler import Txt2ImgGenerator, BackgroundHandler, Img2ImgGenerator
 from .core.prompt_builder import generate_prompts
 
 '''
@@ -35,7 +35,7 @@ def image_generator_main(
 
     logger.info("✅ 이미지 로드 성공.")
 
-    # 2 배경 제거
+    # 2. 배경 제거
     logger.debug(f"🛠️ 배경 제거 시작")
     background_handler = BackgroundHandler()
 
@@ -51,6 +51,16 @@ def image_generator_main(
 
     logger.info("✅ 배경 제거 및 저장 성공.")
 
+    # 2.1. 마스크 이미지 생성
+    logger.debug(f"🛠️ 마스크 이미지 생성 시작")
+    mask_image = background_handler.create_mask_from_alpha(processed_image, filename)
+
+    if mask_image is None:
+        logger.error("❌ 마스크 이미지 생성에 실패했습니다. 처리를 중단합니다.")
+        return False
+
+    logger.info("✅ 마스크 이미지 생성 및 저장 성공.")
+
     # 3. 프롬프트 생성
     logger.debug("🛠️ 프롬프트 생성 시작")
     prompts = generate_prompts(product)
@@ -60,31 +70,48 @@ def image_generator_main(
     else:
         logger.error("❌ 프롬프트 생성 실패")
 
-    # 4. 배경 이미지 생성
-    logger.debug("🛠️ 파이프라인 생성 시작")
-    text_to_img_gen = Txt2ImgGenerator(model_pipeline)
-    background_image, background_image_path = text_to_img_gen.generate_background(prompt=prompts["background_prompt"],
-                                                     negative_prompt=prompts["negative_prompt"])
-
-
-    # 5. 이미지 배경 추가
-    logger.debug(f"🛠️ 이미지 배경 추가 시작")
-    bg_image, bg_filename = image_loader.load_image(
-        image_path=background_image_path,
-    )
-
-    if bg_image is None:
-        logger.error("❌ 배경 이미지 로드에 실패했습니다. 배경 합성을 건너뜁니다.")
-    else:
-        image_with_bg = background_handler.add_image_background(
-            foreground_image=processed_image,
-            background_image=bg_image,
-            original_filename=filename,
-            background_filename=bg_filename,
-            output_dir=output_dir_path
+    # 제품에 배경 이미지 생성하기
+    logger.debug("🛠️ 파이프라인 시작")
+    try:
+        img_2_img_gen = Img2ImgGenerator(model_pipeline)
+        gen_image, image_path = img_2_img_gen.generate_img(
+            prompt=prompts["background_prompt"],
+            init_image=processed_image,
+            mask_image=mask_image,
+            negative_prompt=prompts["negative_prompt"]
         )
-        if image_with_bg is None:
-            logger.error("❌ 이미지 배경 추가에 실패했습니다. 처리를 중단합니다.")
-        logger.info("✅ 이미지 배경 추가 및 저장 성공.")
+        logger.info("✅ 이미지 생성 성공")
+    except Exception as e:
+        logger.error(f"❌ 이미지 생성 중 에러 발생: {e}")
 
-    return {"image": image_with_bg}
+    return {"image": gen_image}
+
+
+    # # 4. 배경 이미지 생성
+    # logger.debug("🛠️ 파이프라인 생성 시작")
+    # text_to_img_gen = Txt2ImgGenerator(model_pipeline)
+    # background_image, background_image_path = text_to_img_gen.generate_background(prompt=prompts["background_prompt"],
+    #                                                  negative_prompt=prompts["negative_prompt"])
+
+
+    # # 5. 이미지 배경 추가
+    # logger.debug(f"🛠️ 이미지 배경 추가 시작")
+    # bg_image, bg_filename = image_loader.load_image(
+    #     image_path=background_image_path,
+    # )
+
+    # if bg_image is None:
+    #     logger.error("❌ 배경 이미지 로드에 실패했습니다. 배경 합성을 건너뜁니다.")
+    # else:
+    #     image_with_bg = background_handler.add_image_background(
+    #         foreground_image=processed_image,
+    #         background_image=bg_image,
+    #         original_filename=filename,
+    #         background_filename=bg_filename,
+    #         output_dir=output_dir_path
+    #     )
+    #     if image_with_bg is None:
+    #         logger.error("❌ 이미지 배경 추가에 실패했습니다. 처리를 중단합니다.")
+    #     logger.info("✅ 이미지 배경 추가 및 저장 성공.")
+
+    # return {"image": image_with_bg}
