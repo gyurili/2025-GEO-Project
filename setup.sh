68e502c6d4f2f282ac29613c0b17de903fff3afa
gyurili <inglifestora@naver.com>
2025-07-22 02:00:30 +0000

    fix: setup 명령어 수정

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+# 시스템 의존성
+sudo apt update
+sudo apt install -y wkhtmltopdf fonts-noto-cjk build-essential python3.10-dev
+
+# Python 패키지
 pip install -r requirements.txt
+
+# Playwright 설치
 playwright install chromium
 python -m playwright install-deps
